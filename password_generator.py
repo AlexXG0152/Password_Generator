309efa9cff56de271c940c726b8a45b4c2dc80f8
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2020-12-22 19:40:40 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -7,29 +7,45 @@ Here is my implementation of this task.
 Due to the fact that in some fonts they are very similar to each other and this can cause difficulties for the user.
 2. Also, I implemented a simple check that the user should enter a positive number of the desired password length,
 and not a text, letter, negative number, float number.
+3. Also, verification is built into password generation function. Thus, it can be used in a loop.
 """
 
-letters = "23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ!#$%&"
+symbols = "23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ!#$%&"
 
+def ask_user():
+    """
+    We ask user how long password he needs and check his input.
+    """
+    password_lenght = 0
+    while password_lenght == 0:
+        try:
+            password_lenght = int(input("How long password you want? Enter the number... "))
+            if password_lenght <= 0:
+                print("Try to enter any number greater than 0...")
+                continue
+            return password_lenght
+        except Exception:
+            continue
 
-def password_generator():
+    
+def password_generator(password_lenght):
+    """
+    Checking input data and generating password of a given length.
+    """
     password = ""
-    password_lenght = input("How long password you want? Enter the number... ")
 
     try:
-        password_lenght = int(password_lenght)
-        if password_lenght > 0:
-            for _ in range(password_lenght):
-                choice = random.choice(letters)
+        if password_lenght >=1:
+            for i in range(password_lenght):
+                choice = random.choice(symbols)
                 password += str(choice)
-            print(f"Your password is:  {password}")
-            print("Thank you!")
+            print(f"Your password is:  {password} \nTnank you!")
+            return password
         else:
-            print("Try to enter any number greater than 0...")
-            password_generator()
-    except ValueError:
-        password_generator()
-    return password
+            return 0
+    except Exception:
+        pass
 
 
-password_generator()
+password_lenght = ask_user()
+password_generator(password_lenght)
