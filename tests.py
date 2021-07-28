2d4914e332d2272d4ea58a400da372d856cc4aa5
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2021-07-28 21:26:42 +0300

    Tests for functions
    Very simple tests

---
+++ tests.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+import unittest
+
+from pass_gen import ask_user, password_generator
+
+class PasswordsTestCase(unittest.TestCase):
+
+    def test_get_input_processed_input_correctly(self):
+        user_input = "7"
+        expected_input = 7
+        with patch('builtins.input', side_effect=user_input):
+            user_in = ask_user()
+        self.assertEqual(user_in, expected_input)
+
+    def test_answer(self):
+        expected_pass_len = 7
+        with patch('builtins.input'):
+            gen = password_generator(7)
+        self.assertEqual(len(gen), expected_pass_len)
+
+
+if __name__ == '__main__':
+    unittest.main()
